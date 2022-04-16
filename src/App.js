74b32d5d013415c9cb88ab338dd3d111c98b0cc0
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-17 02:14:10 +0600

    Update about and login component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 
@@ -10,6 +12,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const About = () => {
+  return (
+    <Container className="text-center py-5">
+      <h1>Working...</h1>
+    </Container>
+  );
+};
+
+export default About;
