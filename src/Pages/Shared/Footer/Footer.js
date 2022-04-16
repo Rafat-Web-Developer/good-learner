c740a0b21db50be00b2db788a4fbe0b331da1f2c
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-17 01:36:09 +0600

    Update footer design

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-success py-5">
+    <footer className="bg-success pt-5 pb-2">
       <p className="text-center text-white">
         Developed By : <span className="text-info fw-bold">Rafat Hossain</span>
       </p>
