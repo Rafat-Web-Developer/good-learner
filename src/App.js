3d13e55a8a430cbe24797d38c40d14add572adb8
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-17 02:46:05 +0600

    Create a checkout component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./Pages/About/About";
 import Blogs from "./Pages/Blogs/Blogs";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import Checkout from "./Pages/Protected/Checkout/Checkout";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 import SignUp from "./Pages/SignUp/SignUp";
@@ -18,6 +19,7 @@ function App() {
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signup" element={<SignUp></SignUp>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/checkout" element={<Checkout></Checkout>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Protected/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <div className="text-center py-5">
+      <h1>Checkout working...</h1>
+    </div>
+  );
+};
+
+export default Checkout;
