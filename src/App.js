d6bad8dcff8745b842c548b6f4faf48288eb1004
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-17 00:58:17 +0600

    Create some folder and design Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Good Learner</h1>
+    <div>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <section>
+        <h1>Banner</h1>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
