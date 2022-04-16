70c872fdbf870ea055d9fa92ad4a683252291d1a
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-17 01:03:22 +0600

    Design Footer component and use it in app

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-success py-5">
+      <p className="text-center text-white">
+        Developed By : <span className="text-info fw-bold">Rafat Hossain</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
