208546af3517b338b39a17c2aada353e40f7fcdc
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-18 13:05:13 +0600

    Update question and answer

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -5,18 +5,23 @@ import Question from "./Ouestion/Question";
 const questions = [
   {
     id: 1,
-    question: "Question 1",
-    answer: "Answer 1",
+    question: "Difference between authorization and authentication",
+    answer:
+      "Authorization is the process of checking what specific applications, files, and data a user has access to permission, but Authentication is the process of verifying user is valid or not.",
   },
   {
     id: 2,
-    question: "Question 2",
-    answer: "Answer 2",
+    question:
+      "Why are you using firebase? What other options do you have to implement authentication?",
+    answer:
+      "There several reason to use firebase authentication. These are firebase provides backend services, easy-to-use, and ready-made UI libraries to authenticate users to your app, and it's most secure. Some ALternative options of firebase for implement authentication are Back4app, Parse, AWS Amplify, Backendless, Kuzzle, and so on. You also create a backend application using laravel.",
   },
   {
     id: 3,
-    question: "Question 3",
-    answer: "Answer 3",
+    question:
+      "What other services does firebase provide other than authentication?",
+    answer:
+      "Cloud Firestore, Hosting, Cloud Storage, Google Analytics, Database, Predictions, Cloud Messaging and so on.",
   },
 ];
 
